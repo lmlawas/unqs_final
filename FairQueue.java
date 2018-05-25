cf1030ff6b0ed76ff41f2e714ded73351ba24e3f
Leensey Lawas <lmlawas@up.edu.ph>
2018-05-25 14:39:53 +0800

    Move FQ location temp

---
+++ FairQueue.java
@@ -0,0 +1,125 @@
+import java.io.*;
+import java.util.LinkedList;
+
+public class FairQueue implements Schedule {
+
+	/* Attributes */	
+	public int packets_dropped_cnt;
+	public int packets_switched_cnt;
+	public int total_wait_time;
+	public long packets_dropped_size;
+	public long packets_switched_size;
+	public LinkedList<NetworkBuffer> buffer;
+
+	/* Constructor */
+	public FairQueue() {
+		// for throughput
+		packets_dropped_size = 0;
+		packets_switched_size = 0;
+
+		// for total packets
+		packets_dropped_cnt = 0;
+		packets_switched_cnt = 0;
+
+		// for average wait time
+		total_wait_time = 0;
+
+		// for buffers
+		buffer = new LinkedList<NetworkBuffer>();
+		for(int i=0;i<3;i++){
+			buffer.add(new NetworkBuffer());
+		}
+	}
+
+	/* Methods */
+	public double throughput(int duration) {
+		return (packets_switched_size / duration);
+	}
+
+	public void addPacket(Packet p) {
+		buffer.get(p.priority).add(p);
+	}
+
+	public void dropPacket(Packet p) {
+		buffer.get(p.priority).remove();
+		packets_dropped_size += p.size;
+		packets_dropped_cnt++;
+	}
+
+	public void info(int bandwidth, int duration, String dateAsText) {
+		System.out.println("\n\n------[ RESULT ]------\n");
+		System.out.println("TIMESTAMP = " + dateAsText);
+		System.out.println("BANDWIDTH = " + bandwidth + " bps\n");
+		System.out.println("packets_dropped_size = " + packets_dropped_size + " b");
+		System.out.println("packets_dropped_cnt = " + packets_dropped_cnt + " packets\n");
+		System.out.println("packets_switched_size = " + packets_switched_size + " b");
+		System.out.println("packets_switched_cnt = " + packets_switched_cnt + " packets\n");
+		System.out.println("total_wait_time = " + total_wait_time + " seconds");
+		System.out.println("throughput = " + throughput(duration) + " bps");
+	}
+
+	public void saveResults(int bandwidth, int duration, String dateAsText) throws IOException {
+		FileWriter fw = new FileWriter("results_fq.txt", true);		
+		fw.write("\n\n------[ RESULT ]------");
+		fw.write("\nTIMESTAMP = " + dateAsText);
+		fw.write("\nBANDWIDTH = " + bandwidth + " bps\n");
+		fw.write("\n\tpackets_dropped_size = " + packets_dropped_size + " b");
+		fw.write("\n\tpackets_dropped_cnt = " + packets_dropped_cnt + " packets\n");
+		fw.write("\n\tpackets_switched_size = " + packets_switched_size + " b");
+		fw.write("\n\tpackets_switched_cnt = " + packets_switched_cnt + " packets\n");
+		fw.write("\n\ttotal_wait_time = " + total_wait_time + " seconds");		
+		fw.write("\n\tthroughput = " + throughput(duration) + " bps");
+		fw.close();
+	}
+
+	public void switchPacket(Packet p) {
+		buffer.get(p.priority).remove();
+		packets_switched_size += p.size;
+		packets_switched_cnt++;
+	}
+
+	public void process(int bandwidth, int current_time, int timeout, LinkedList<Packet> packets) {
+		int temp_buffer_size = 0,
+		    temp_priority = 0;
+
+		for (Packet p : packets) {
+			// p.info();
+			addPacket(p);
+		}
+
+		while (!emptyBuffers()) {
+			temp_priority = getPriorityWithMinVFT(current_time);
+			Packet p = buffer.get(temp_priority).peekFirst();
+			if (p.waitTime(current_time) == timeout) {
+				dropPacket(p);
+				total_wait_time += p.waitTime(current_time);
+			} else if (p.size + temp_buffer_size <= bandwidth) {
+				switchPacket(p);
+				total_wait_time += p.waitTime(current_time);
+			} else break;
+		}
+	}
+
+	int getPriorityWithMinVFT(int current_time) {
+		int temp_priority = 0;
+		long min_vft = Integer.MAX_VALUE;
+		     
+		for (NetworkBuffer temp_buffer : buffer) {
+			if (!temp_buffer.isEmpty()) {
+				Packet p = temp_buffer.peekFirst();
+				if (p.virtualFinishTime(current_time) < min_vft) {
+					min_vft = p.virtualFinishTime(current_time);
+					temp_priority = p.priority;
+				}
+			}
+		}
+		return temp_priority;
+	}
+
+	boolean emptyBuffers() {
+		for (NetworkBuffer temp_buffer : buffer) {
+			if (temp_buffer.isEmpty()) return true;
+		}
+		return false;
+	}
+}
